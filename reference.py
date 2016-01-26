62b3198bdbb670b851e68866ec179847bbf26542
Arvind Jayaprakash <work@anomalizer.net>
2016-01-26 11:03:57 +0530

    Headers done

--- reference.py
+++ reference.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+from datetime import datetime
+from hashlib import sha1
+import hmac
+import sys
+
 
 '''
 Authorization = "AWS" + " " + AWSAccessKeyId + ":" + Signature;
@@ -26,6 +31,37 @@ def canon_resource(vhost_mode, bucket, url):
 
     return val
 
+
+def str_to_sign(method, vhost_mode, bucket, url):
+    cr = canon_resource(vhost_mode, bucket, url)
+    ctype = ""
+    cmd5 = ""
+    dt = datetime.utcnow().isoformat('T')+"Z"
+    azh = ""
+    retval = "%s\n%s\n%s\n%s\n%s%s" % (method,
+        cmd5, ctype, dt, azh, cr)
+    headers = {}
+    headers['Date'] = dt
+    if vhost_mode:
+        headers['Host'] = bucket
+    return {'s2s': retval, 'headers': headers }
+
+
+def sign(key, method, vhost_mode, bucket, url):
+    raw = str_to_sign(method, vhost_mode, bucket, url)
+    retval = hmac.new(key, raw['s2s'], sha1)   
+    return {'sign': retval.digest().encode("base64").rstrip("\n"),
+        'headers': raw['headers']}
+
+def az_h(ak, key, method, vhost_mode, bucket, url):
+    sig = sign(key, method, vhost_mode, bucket, url)
+    ahv = "AWS %s:%s" % (ak, sig['sign'])
+    sig['headers']['Authorization'] = ahv
+    return sig['headers']
+
+
 if __name__ == "__main__":
-    print canon_resource(True, "bugait", "/shash")
-    print canon_resource(False, "bugait", "/bugait/shash")
+    ak = sys.argv[1]
+    k = sys.argv[2]
+    print az_h(ak, k, "GET", True, "hw.anomalizer", "/lock.txt")
+    print az_h(ak, k, "GET", False, "hw.anomalizer", "/hw.anomalizer/nq.c")
